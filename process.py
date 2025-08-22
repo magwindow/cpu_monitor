45b50591200d54a537fef8b00b53ebab9490f707
magwindow <antosha.petrakov@yandex.ru>
2025-08-22 11:58:17 +0300

    Added method cpu_percent_return

--- process.py
+++ process.py
@@ -3,8 +3,18 @@ import psutil as pt
 
 class CpuBar:
     def __init__(self):
-        self.cpu_count = pt.cpu_count(logical=False)
-        self.cpu_count_logical = pt.cpu_count()
+        self.cpu_count = pt.cpu_count(logical=False)  # количество ядер
+        self.cpu_count_logical = pt.cpu_count()  # количество процессов
+
+    def cpu_percent_return(self):
+        """Возвращает загрузку процессов"""
+        return pt.cpu_percent(percpu=True)
+
+
+x = CpuBar()
+
+for _ in range(10):
+    print(x.cpu_percent_return())
 
 
 
