665742af3d51b9f17c59c235c6a8a186677d8ef4
magwindow <antosha.petrakov@yandex.ru>
2025-08-22 12:26:59 +0300

    Update configure_cpu_bar

--- widget_update.py
+++ widget_update.py
@@ -1,10 +1,11 @@
 class ConfigureWidgets:
     def configure_cpu_bar(self):
         r = self.cpu.cpu_percent_return()  # список с процентами загрузки
-        print(r)
         for i in range(self.cpu.cpu_count_logical):
             self.list_label[i].configure(text=f'core {i + 1} usage: {r[i]}%')
             self.list_pbar[i].configure(value=r[i])
+
+        self.after(1000, self.configure_cpu_bar)  # обновляет прогресс бары в 1 секунду
 
 
 
