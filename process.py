e00ca8b2d59098f8c1608dae8e695be5255f6bbf
magwindow <antosha.petrakov@yandex.ru>
2025-08-22 11:21:32 +0300

    Update class CpuBar

--- process.py
+++ process.py
@@ -4,7 +4,9 @@ import psutil as pt
 class CpuBar:
     def __init__(self):
         self.cpu_count = pt.cpu_count(logical=False)
+        self.cpu_count_logical = pt.cpu_count()
         print(f'Количество ядер процессора: {self.cpu_count}')
+        print(f'Количество потоков: {self.cpu_count_logical}')
 
 
 CpuBar()
