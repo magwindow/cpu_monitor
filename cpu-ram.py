27f4c09a2888fe8721f3cb706fb91000f4ef8cd4
magwindow <antosha.petrakov@yandex.ru>
2025-08-22 09:47:30 +0300

    Create attributes method init

--- cpu-ram.py
+++ cpu-ram.py
@@ -4,7 +4,13 @@ import tkinter as tk
 class Application(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        self.attributes('-alpha', 1)  # непрозрачность 100%
+        self.attributes('-topmost', True)  # расположить поверх окон
+        self.overrideredirect(True)  # убирает рамки
+        self.resizable(False, False)
+        self.title('CPU-RAM')
 
 
-root = Application()
-root.mainloop()
+if __name__ == '__main__':
+    root = Application()
+    root.mainloop()
